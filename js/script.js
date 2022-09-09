14ee0722c20b712c08be29fb6791f08a599d322c
RoninsWork <yurkaronin@gmail.com>
2022-09-09 11:28:55 +0300

    Блок КОНТАКТЫ

--- js/script.js
+++ js/script.js
@@ -124,6 +124,51 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   };
 
+  // Яндекс карта 
+  if (document.querySelector('.js-map')) {
+    // Интерактивная карта с гео меткой
+    let center = [45.066341574598155,38.95656499999995];
+  
+    function init() {
+  
+      let map = new ymaps.Map("map", {
+        center: center, // ваши данные
+        zoom: 17
+      });
+  
+      let placemarRkschool = new ymaps.Placemark([45.066341574598155,38.95656499999995], {}, {
+        iconLayout: 'default#image',
+        /* говорим что будем отображать на карте в качестве геометки  */
+        iconImageHref: './img/icon-map-mark-2.svg',
+        /* указываем пусть к картинке на нашем сайте  */
+        iconImageSize: [40, 56],
+        /* размеры картинки  */
+        iconImageOffset: [-22, -22]
+      });
+  
+      map.controls.remove('geolocationControl'); // удаляем геолокацию
+      map.controls.remove('searchControl'); // удаляем поиск
+      map.controls.remove('trafficControl'); // удаляем контроль трафика
+      map.controls.remove('typeSelector'); // удаляем тип
+      map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+      map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+      map.controls.remove('rulerControl'); // удаляем контрол правил
+      map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+  
+      map.geoObjects.add(placemarRkschool);
+  
+        placemarRkschool.events
+          .add('mouseenter', function (e) {
+            e.get('target').options.set('iconImageHref', './img/icon-map-mark-2.svg');
+          })
+          .add('mouseleave', function (e) {
+            e.get('target').options.set('iconImageHref', './img/icon-map-mark-1.svg');
+          });
+    }
+  
+    ymaps.ready(init);
+  }
+
 });
 
 
